6e7dcd7ef10f18a05d1bb80791ea134239ed720e
root <root@DESKTOP-M86GBUV.localdomain>
2020-11-15 02:00:30 -0600

    working pug restructure

--- pug/routes/admin.js
+++ pug/routes/admin.js
@@ -6,7 +6,8 @@ const router = express.Router()
 const products = []
 
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "views", "add-product.html"))
+    res.render('add-product', { pageTitle: 'pug add ', path: '/admin/add-product' })
+    // res.sendFile(path.join(__dirname, "..", "views", "add-product.html"))
     // next()
 })
 
