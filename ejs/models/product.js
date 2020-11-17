e2a2a661d89de17b2603f47df61f2ae24e18c0ba
pancho1337 <pancho404x@gmail.com>
2020-11-16 18:49:02 -0600

    ejs added controllers and models

--- ejs/models/product.js
+++ ejs/models/product.js
@@ -1,13 +1,31 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(__dirname, '../data', 'products.json')
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            return cb([])
+        }
+        cb(JSON.parse(data))
+    })
+}
 
 module.exports = class Product {
     constructor(title) {
-        this.title = title
+        this.title = title,
+            this.p = path.join(__dirname, '../data', 'products.json')
     }
     save() {
-        products.push(this)
+        getProductsFromFile((products) => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
     }
-    static fetchAll() {
-        return products
+    static fetchAll(cb) {
+        getProductsFromFile(cb)
     }
 }
